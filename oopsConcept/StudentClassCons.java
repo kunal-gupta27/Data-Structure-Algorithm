1023e97c6b91f342d453f0f0c73cec3bf41a1f4b
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2025-12-30 00:00:31 +0530

    learn final & static keyword

--- oopsConcept/StudentClassCons.java
+++ oopsConcept/StudentClassCons.java
@@ -39,27 +39,40 @@ public class StudentClassCons{
         String name;
         int rno;
         double percent;
+        final String schoolName="KCS";  // final keyword use kiya hain
+        //iski value ko hum update nhi kr sakte hain in future;
+
+        private static int numberofStudent; //Using static keyword for how many object are created 
 
         public student(String naam, int rn, double per){  //Constructor hain jo class name ka he hota hain
             name = naam;
             rno = rn;
             percent = per;
+            numberofStudent++;   //static ko use krne kai baad hum sonstructor main add krte numberofstudent ko ++ kai sath
         }
 
         public student(){   //default constructor hota hain
 
         }
+
+        public static  int getnumberofStudent(){
+            return numberofStudent;
+        }
     }
     public static void main(String[] args) {
+        System.out.println(student.getnumberofStudent());
         student s1 = new student("Tarun", 87, 99.5);
-        System.out.println(s1.name);
-        System.out.println(s1.rno);
-        System.out.println(s1.percent);
+        // System.out.println(s1.name);
+        // System.out.println(s1.rno);
+        // System.out.println(s1.percent);
+        System.out.println(s1.numberofStudent);
 
         student s2 = new student("Rachit", 78, 89.7);
-        System.out.println(s1.name);
-        System.out.println(s1.rno);
-        System.out.println(s1.percent);
+        // System.out.println(s1.name);
+        // System.out.println(s1.rno);
+        // System.out.println(s1.percent);
+        System.out.println(s2.numberofStudent);
+
         
                 
         
