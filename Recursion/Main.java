bf4bbc56c3179ceb04b035c31fed8c9cf205f631
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-02-09 10:54:18 +0530

    learning recursion

---
+++ Recursion/Main.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class Main {
+    public static void printIncreasing(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        printIncreasing(n-1);
+        System.out.print(n+" ");
+    }
+
+    public static void printDecreasing(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        System.out.print(n+" ");
+        printDecreasing(n-1);
+    }
+    public static void main(String[] args) {
+        printIncreasing(5);
+        System.out.println();
+        printDecreasing(5);
+    }
+}
