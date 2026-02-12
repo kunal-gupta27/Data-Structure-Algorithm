1b139a44cf0607a9757d34db468b2f7adce7b3da
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-02-12 22:38:05 +0530

    add factorial

--- Recursion/Main.java
+++ Recursion/Main.java
@@ -18,10 +18,15 @@ public class Main {
         System.out.print(n+" ");
         printDecreasing(n-1);
     }
+    public static int factorial(int n){
+        if(n==0) return 1;
+        return n * factorial(n-1);
+    }
     public static void main(String[] args) {
-        printIncreasing(5);
-        System.out.println();
-        printDecreasing(5);
+        // printIncreasing(5);
+        // System.out.println();
+        // printDecreasing(5);
+        System.out.println(factorial(5));
     }
 
     
