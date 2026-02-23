f38c89aad68b690c33a7b58929003b70b3288470
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-02-23 12:05:54 +0530

    Reverse k Groupes in linkedlist

---
+++ LinkedList_Question/Reversekgroup.java
@@ -0,0 +1,62 @@
+package LinkedList_Question;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+public class Reversekgroup {
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1) return head;
+
+        ListNode curr = head;
+        int count = 0;
+
+        while (curr != null && count < k) {
+            curr = curr.next;
+            count++;
+        }
+
+        if (count == k) {
+            curr = reverseKGroup(curr, k);
+
+            while (count > 0) {
+                ListNode next = head.next;
+                head.next = curr;
+                curr = head;
+                head = next;
+                count--;
+            }
+            head = curr;
+        }
+
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        int k = 2;
+
+        printList(head);
+        head = reverseKGroup(head, k);
+        printList(head);
+    }
+}
