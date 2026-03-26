d1ed2185ac690a3176930c13e65ed1a787b0cd6b
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-03-26 21:43:17 +0530

    traverse in array using recursion

---
+++ Recursion/TraverseArray.java
@@ -0,0 +1,13 @@
+public class TraverseArray {
+    public static void printArray(int[] arr, int idx){
+        if(idx==arr.length){
+            return;
+        }
+        System.out.print(arr[idx]+" ");
+        printArray(arr, idx+1);
+    }
+    public static void main(String[] args) {
+        int[] arr={5, 6, 7, 8};
+        printArray(arr, 0);
+    }
+}
