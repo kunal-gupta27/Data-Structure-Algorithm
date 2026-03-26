2586ca1b41efd8f093d47eb280b5b751da768b25
𝐾𝑢𝑛𝑎𝑙 𝐺𝑢𝑝𝑡𝑎 <kunalgupta.cse2027@gmail.com>
2026-03-26 21:14:03 +0530

    Update Solution.java

--- Hashmap/Solution.java
+++ Hashmap/Solution.java
@@ -5,7 +5,7 @@ class Solution {
         int count = 0;
         int candidate = 0;
 
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0;i<nums.length;i++) {
 
             if(count == 0) {
                 candidate = nums[i];
