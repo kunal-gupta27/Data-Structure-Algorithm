725a79e412d930ba44aa1e893b4b3fed778b41d5
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-05-20 11:26:56 +0530

    Basic Implementation of tree

---
+++ Trees/BasicImpementation.java
@@ -0,0 +1,32 @@
+package Trees;
+
+public class BasicImpementation {
+    public static class Node{
+        int val;
+        Node left;
+        Node right;
+        public Node(int val){
+            this.val=val;
+        }
+    }
+
+    public static void display(Node root){
+
+    }
+
+
+    public static void main(String[] args) {
+        Node a = new Node(1);  //a is the root
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        Node g = new Node(7);
+
+        a.left=b; a.right=c;
+        b.left=d; b.right=e;
+        c.left=f; c.right=g;
+
+    }
+}
