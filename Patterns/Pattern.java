fc56ee067eb0e78a902368e9368d351d189ee6ba
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-06-19 10:50:04 +0530

    pattern printing here

---
+++ Patterns/Pattern.java
@@ -0,0 +1,76 @@
+package Patterns;
+
+import java.util.*;
+
+public class Pattern {
+    public static void pattern2(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern3(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern4(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern5(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern6(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern7(int n){
+        for(int i=0;i<n;i++){
+            //spaces
+            for(int j=1;j<=n-i-1;j++){
+                System.out.print(" ");
+            }
+
+            //star
+            for(int j=1;j<=2*i+1;j++){
+                System.out.print("*");
+            }
+
+            //spaces
+            for(int j=1;j<=n-i-1;j++){
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+    public static void pattern8(int n){
+        for(int i=0;i<n;i++){
+
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern8(n);
+    }
+}
